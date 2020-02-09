7f3f3357a3f793b61036bd8098aee74de7f1c2da
Ha-g <you@example.com>
2020-02-08 18:07:46 -0800

    Animation mostly complete

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const state = {
     game :1, 
     gameOver : 2
 }
+const degree = Math.PI/180;
 document.addEventListener('click', function(e){
     switch(state.current) {
         case state.getReady:
@@ -34,7 +35,8 @@ const bg = {
     y:canvas.height - 226,
 
     draw: function() {
-        canvasContext.drawImage(sprite, this.sx, this.sy, this.w, this.h, this.x, this.y, this.x +  this.w + 14, this.y)
+        canvasContext.drawImage(sprite, this.sx, this.sy, this.w, this.h, this.x , this.y, this.w, this.h)
+        canvasContext.drawImage(sprite, this.sx, this.sy, this.w, this.h, this.x + this. w, this.y, this.w, this.h)
     }
     
 }
@@ -42,17 +44,28 @@ const fg = {
     sx:276,
     sy:0,
     w:224,
-    h:275,
+    h:112,
     x:0,
-    y:canvas.height - 100,
+    y:canvas.height - 112,
+
+    dx: 2,
+    update: function() {
+if (state.current == state.game) {
+    this.x = (this.x - this.dx ) % (this.w / 2)
+}
+    },
 
     draw: function() {
-        canvasContext.drawImage(sprite, this.sx, this.sy, this.w, this.h, this.x, this.y, this.x +  this.w + 65, this.y)
+        canvasContext.drawImage(sprite, this.sx, this.sy, this.w, this.h, this.x, this.y, this.w, this.h)
+        
+        canvasContext.drawImage(sprite, this.sx, this.sy, this.w, this.h, this.x + this. w, this.y, this.w, this.h)
     }
     
 }
 
 const bird = {
+    
+
     animation : [
         {sX:276, sY:112},
         {sX:276, sY:139},
@@ -66,20 +79,58 @@ const bird = {
 
     frame : 0,
 
+    speed : 0,
+    gravity : 0.25,
+    jump : 4.6,
+    rotation : 0,
+
     draw : function() {
         let bird = this.animation[this.frame]
-        canvasContext.drawImage(sprite, bird.sX, bird.sY, this.w, this.h , this.x - this.w/2, this.y - this.y/2 , this.w, this.h)
+        
+        canvasContext.save();
+        
+        canvasContext.translate(this.x, this.y)
+        
+        canvasContext.rotate(this.rotation)
+        
+        canvasContext.drawImage(sprite, bird.sX, bird.sY, this.w, this.h , - this.w/2, - this.h/2 , this.w, this.h)
 
+        canvasContext.restore()
     },
     update : function() {
+        
         this.period = state.current == state.getReady ? 10 : 5
 
         this.frame += frames%this.period == 0 ? 1 : 0
 
         this.frame = this.frame%this.animation.length;
+
+        if(state.current == state.getReady) {
+            this.y = 150
+            this.rotation = 0 * degree
+        }else {
+            this.speed += this.gravity;
+            this.y += this.speed
+
+            if ( this.y + this.h/2 >= canvas.height - fg.h ) {
+                this.y = canvas.height - fg.h - this.h/2
+                if(state.current == state.game) {
+    
+                    state.current = state.gameOver
+                }
+            }
+            if (this.speed >= this.jump) {
+                this.rotation = 90 * degree
+                this.frame = 1
+            }else {
+                this.rotation = -25 * degree
+            }
+        }
+        // console.log(fg.h)
+        // console.log(canvas.height -fg.h)
     },
     flap : function() {
-
+        this.speed =- this.jump
     }
 }
 
@@ -129,6 +180,7 @@ function draw() {
 }
 function update() {
     bird.update()
+    fg.update();
 }
 function loop() {
     update()
